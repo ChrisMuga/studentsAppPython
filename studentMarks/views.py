46234808665d4b10aace6ef7d628bf4ebd47840d
Chris Muga <ChrisMuga94@gmail.com>
2019-07-19 11:00:11 +0300

    register new user using User model

--- studentMarks/views.py
+++ studentMarks/views.py
@@ -2,16 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
 
+app_templates = {
+    'student_registration': 'studentMarks/index.html'
+}
+
 
 def index(request):
-    appTemplates = {
-        'student_registration': 'studentMarks/index.html'
-    }
     context = "Student Registration"
-    return render(request, appTemplates["student_registration"], {
+    return render(request, app_templates["student_registration"], {
         'context': context
     })
 
 
 def new_user(request):
-    return HttpResponse(request)
+
+    first_name = request.POST.get("firstName")
+    last_name = request.POST.get("lastName")
+    current_class = request.POST.get("currentClass")
+    email_address = request.POST.get("emailAddress")
+    date_of_birth = request.POST.get("dateOfBirth")
+    current_stream = request.POST.get("currentStream")
+
+    register_user = User(
+        firstName=first_name,
+        lastName=last_name,
+        currentClass=current_class,
+        emailAddress=email_address,
+        dateOfBirth=date_of_birth,
+        currentStream=current_stream
+    )
+
+    register_user.save()
+
+    return HttpResponse(register_user.id)
