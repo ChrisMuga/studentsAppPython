13bc0627ecc0b5bf7833b30dc3568073d9898cc2
Chris Muga <ChrisMuga94@gmail.com>
2019-07-19 10:34:26 +0300

    create templates dictionary

--- studentMarks/urls.py
+++ studentMarks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new-user', views.new_user)
 ]
--- studentMarks/views.py
+++ studentMarks/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
 
 
 def index(request):
+    appTemplates = {
+        'student_registration': 'studentMarks/index.html'
+    }
     context = "Student Registration"
-    return render(request, 'studentMarks/index.html', {
+    return render(request, appTemplates["student_registration"], {
         'context': context
     })
+
+
+def new_user(request):
+    return HttpResponse(request)
